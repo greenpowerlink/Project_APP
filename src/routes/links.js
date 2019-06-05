8e546ee0dcfedbe7ba229e63992f8d4b04fcbc02
DanyCaro <50565029+DanyCaro@users.noreply.github.com>
2019-06-05 15:53:22 +0800

    working on mysql
    http://localhost:3000/links/add
    http://localhost:3000/links
    http://localhost:3000/links/list

--- src/routes/links.js
+++ src/routes/links.js
@@ -6,13 +6,17 @@ router.get('/add', (req, res) => {
     res.render('links/add');
 });
 
-router.post('/add', (req, res) => {
-    res.send('Received');
+router.post('/add', async(req, res) => {
+    console.log(res);
+    const newfile = await pool.query("DELETE FROM `table 4` WHERE `COL 1`='Job', `COL 1`=''; ");
+    console.log(newfile);
+    res.redirect('/links');
 });
 
 router.get('/', async(req, res) => {
     const links = await pool.query('SELECT * FROM employee');
     res.render('links/list', { links });
+    console.log(links);
 
 });
 
